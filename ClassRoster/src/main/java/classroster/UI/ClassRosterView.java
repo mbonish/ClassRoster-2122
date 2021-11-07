497273828f464bbeb8a4a111a85fb8a193e8a0a7
Mariana Bonish <51426161+mbonish@users.noreply.github.com>
2021-11-07 13:25:25 -0600

    refactor

--- ClassRoster/src/main/java/classroster/UI/ClassRosterView.java
+++
@@ -1,8 +0,0 @@
-
-import classroster.dto.Student;
-
-public class ClassRosterView {
-    public Student getNewStudentInfo(){
-        String studentId = io.readString("Please enter Student ID");
-    }
-}
