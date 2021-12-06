4a2f07662e563fd43a4d1acdf2f59600432e8768
Mariana <marianabonish88@gmail.com>
2021-12-06 17:28:18 -0600

    new

---
+++ ClassRoster/src/main/java/ClassRoster/dao/ClassRosterDaoException.java
@@ -0,0 +1,11 @@
+package ClassRoster.dao;
+
+public class ClassRosterDaoException extends Exception {
+    public ClassRosterDaoException(String message) {
+        super(message);
+    }
+    public ClassRosterDaoException(String message, Throwable cause) {
+        super(message, cause);
+        }
+
+}
--- ClassRoster/src/main/java/ClassRoster/dao/ClassRosterDaoFileImpl.java
+++ ClassRoster/src/main/java/ClassRoster/dao/ClassRosterDaoFileImpl.java
@@ -2,15 +2,16 @@ package ClassRoster.dao;
 
 import ClassRoster.dto.Student;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
 
 public class ClassRosterDaoFileImpl implements ClassRosterDao {
 
     private Map<String, Student> students = new HashMap<>();
-
+    public static final String ROSTER_FILE = "roster.txt";
+    public static final String DELIMITER = "::";
 
     /**
      * Adds the given Student to the roster and associates it with the given
@@ -38,7 +39,7 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
      * in the roster
      */
     @Override
-    public List<Student> getAllStudents(){
+    public List<Student> getAllStudents() {
         return new ArrayList<Student>(students.values());
     }
 
@@ -50,12 +51,11 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
      * @param studentId ID of the student to retrieve
      * @return String array containing the ids of all the students
      * in the roster
-   */
+     */
     @Override
     public Student getStudent(String studentId) {
         return students.get(studentId);
     }
-
 
 
     /**
@@ -69,9 +69,86 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
      */
     @Override
     public Student removeStudent(String studentId) {
-        Student removedStudent =students.remove(studentId);
+        Student removedStudent = students.remove(studentId);
         return removedStudent;
     }
 
+    //This method it retrieving the student information from the text file then using
+    //pieces to create a new student object
+    private Student unmarshallStudent(String studentAsText) {
+        // studentAsText is expecting a line read in from our file.
+        // For example, it might look like this:
+        // 1234::Ada::Lovelace::Java-September1842
+        //
+        // We then split that line on our DELIMITER - which we are using as ::
+        // Leaving us with an array of Strings, stored in studentTokens.
+        // Which should look like this:
+        // ______________________________________
+        // |    |   |        |                  |
+        // |1234|Ada|Lovelace|Java-September1842|
+        // |    |   |        |                  |
+        // --------------------------------------
+        //  [0]  [1]    [2]         [3]
+
+
+        String[] studentTokens = studentAsText.split(DELIMITER);
+
+        // Given the pattern above, the student Id is in index 0 of the array.
+        String studentId = studentTokens[0];
+
+        // Which we can then use to create a new Student object to satisfy
+        // the requirements of the Student constructor.
+        Student studentFromFile = new Student(studentId);
+
+
+        // However, there are 3 remaining tokens that need to be set into the
+        // new student object. Do this manually by using the appropriate setters.
+
+        //Index 1 - First Name
+        studentFromFile.setFirstName(studentTokens[1]);
+
+        //Index 2- Last Name
+        studentFromFile.setLastName(studentTokens[2]);
+
+        //Index 3- Student Cohort
+        studentFromFile.setCohort(studentTokens[3]);
+
+        //We have now created the final student and can return it
+        return studentFromFile;
+    }
+
+    private void loadRoster() throws ClassRosterDaoException {
+        Scanner scanner;
+
+        try {
+            scanner = new Scanner(
+                    new BufferedReader(
+                            new FileReader(ROSTER_FILE)));
+        } catch (FileNotFoundException e) {
+            throw new ClassRosterDaoException(
+                    "-_- Could not load roster data into memory.", e);
+        }
+
+        //currentLine holds the most recent line read from the file
+        String currentLine;
+        //currentStudent holds the most recent student unmarshalled
+        Student currentStudent;
+        //Go through ROSTER_FILE line by line , decoding each line into a
+        //Student object by calling the unmarshalled student method.
+        //process while we have more lines in the file
+        while (scanner.hasNextLine()) {
+            //unmarshall the line into a student
+            currentLine = scanner.nextLine();
+            currentStudent = unmarshallStudent(currentLine);
+
+            // We are going to use the student id as the map key for our student object.
+            // Put currentStudent into the map using student id as the key
+            students.put(currentStudent.getStudentId(), currentStudent);
+        }
+        scanner.close();
+    }
 }
+
+
+
 
